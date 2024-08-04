b9671265d19a4916234040719db75baf1a1dc180
ÇÆLVÏÑË ØBÜ-MD___TECH <calvinonyango452@gmail.com>
2024-08-04 23:48:20 +0300

    Update id.js

--- id.js
+++ id.js
@@ -1,4 +1,4 @@
-function makeid(num = 4) {
+function makeid(num = 3) {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
